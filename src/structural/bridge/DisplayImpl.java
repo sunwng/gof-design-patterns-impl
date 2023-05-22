d08e3f9527b47162f1c7aa359c3822914608444c
sunwng <sunwgna@gmail.com>
2023-05-22 22:20:06 +0900

    bridge pattern

---
+++ src/structural/bridge/DisplayImpl.java
@@ -0,0 +1,10 @@
+package structural.bridge;
+
+public abstract class DisplayImpl {
+
+    public abstract void rawOpen();
+
+    public abstract void rawPrint();
+
+    public abstract void rawClose();
+}

79d1a3db0378f4f3159d27abc92ba05510dd0504
sunwng <sunwgna@gmail.com>
2023-06-19 09:04:19 +0900

    flyweight pattern

---
+++ src/structural/flyweight/BigString.java
@@ -0,0 +1,20 @@
+package structural.flyweight;
+
+public class BigString {
+    private BigChar[] bigChars;
+
+    public BigString(String string) {
+        BigCharFactory factory = BigCharFactory.getInstance();
+        bigChars = new BigChar[string.length()];
+        for (int i = 0; i < bigChars.length; i++) {
+            bigChars[i] = factory.getBigChar(string.charAt(i));
+        }
+    }
+
+    public void print() {
+        for (BigChar bc : bigChars) {
+            bc.print();
+        }
+    }
+
+}
---
+++ src/structural/flyweight/Main.java
@@ -0,0 +1,10 @@
+package structural.flyweight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BigString bs = new BigString("123190");
+        bs.print();
+    }
+
+}
